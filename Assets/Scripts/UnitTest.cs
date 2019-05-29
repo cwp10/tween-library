1d695079236040b498ad9cd3ef823b4aca7d83fe
Won Pyoung Choi <cwp10@msn.com>
2019-05-30 00:04:41 +0900

    애니매이션 커브 유닛 테스트

---
+++ Assets/Scripts/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tween.Curve;
+
+public class UnitTest : MonoBehaviour
+{
+    public AnimationCurve curve = null;
+
+    private void Awake()
+    {
+        curve = CurveGenerator.Create(CurveType.BounceEaseInOut);
+    }
+}
