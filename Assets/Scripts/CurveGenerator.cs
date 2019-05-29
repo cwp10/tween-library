c0bdac81593fc9ddf930493f809a41a46973b990
Won Pyoung Choi <cwp10@msn.com>
2019-05-30 00:57:52 +0900

    CurveType 스크립트 분리

--- Assets/Scripts/CurveGenerator.cs
+++ Assets/Scripts/CurveGenerator.cs
@@ -3,61 +3,6 @@ using UnityEngine;
 
 namespace Tween.Curve
 {
-    public enum CurveType
-    {
-        Linear,
-
-        ExpoEaseOut,
-        ExpoEaseIn,
-        ExpoEaseInOut,
-        ExpoEaseOutIn,
-
-        CircEaseOut,
-        CircEaseIn,
-        CircEaseInOut,
-        CircEaseOutIn,
-
-        QuadEaseOut,
-        QuadEaseIn,
-        QuadEaseInOut,
-        QuadEaseOutIn,
-
-        SineEaseOut,
-        SineEaseIn,
-        SineEaseInOut,
-        SineEaseOutIn,
-
-        CubicEaseOut,
-        CubicEaseIn,
-        CubicEaseInOut,
-        CubicEaseOutIn,
-
-        QuartEaseOut,
-        QuartEaseIn,
-        QuartEaseInOut,
-        QuartEaseOutIn,
-
-        QuintEaseOut,
-        QuintEaseIn,
-        QuintEaseInOut,
-        QuintEaseOutIn,
-
-        ElasticEaseOut,
-        ElasticEaseIn,
-        ElasticEaseInOut,
-        ElasticEaseOutIn,
-
-        BounceEaseOut,
-        BounceEaseIn,
-        BounceEaseInOut,
-        BounceEaseOutIn,
-
-        BackEaseOut,
-        BackEaseIn,
-        BackEaseInOut,
-        BackEaseOutIn
-    }
-
     public class CurveGenerator
     {
         private delegate double Easing(double time, double min, double max, double duration);
