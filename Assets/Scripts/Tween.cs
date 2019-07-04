4d6dc4dde7da5d25e5a33b5ff2367d56d70063a6
Won Pyoung Choi <cwp10@msn.com>
2019-07-04 17:22:08 +0900

    이너클래스를 별도로 분리하고 딜레이 변수 추가

---
+++ Assets/Scripts/Tween.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Tween
+{
+    private Transform _target = null;
+    private TweenType _type = TweenType.LocalPosition;
+    private Vector3 _from = Vector3.zero;
+    private Vector3 _to = Vector3.zero;
+    private Action _callback = null;
+    private AnimationCurve _curve = null;
+    private float _duration = 0.0f;
+    private float _delay = 0.0f;
+    private bool _isPlaying = false;
+
+    public Tween(Transform target, TweenType type, Vector3 to, float duration, Action callback = null, EasingType easyType = EasingType.Linear, float delay = 0)
+    {
+        this._target = target;
+        this._type = type;
+        this._to = to;
+        this._duration = duration;
+        this._callback = callback;
+        this._curve = CurveGenerator.Create(easyType);
+        this._delay = delay;
+    }
+
+    public void AddListener(Action callback)
+    {
+        _callback += callback;
+    }
+
+    public bool IsPlaying()
+    {
+        return _isPlaying;
+    }
+
+    public void Pause()
+    {
+        _isPlaying = false;
+    }
+
+    public void Resume()
+    {
+        _isPlaying = true;
+    }
+
+    public IEnumerator Execution()
+    {
+        _isPlaying = true;
+        this._from = GetFrom();
+        float playTimer = 0.0f;
+        float delayTimer = 0.0f;
+
+        while (delayTimer <= this._delay)
+        {
+            if (_isPlaying) delayTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        while (playTimer <= this._duration)
+        {
+            Lerp(this._from, this._to, this._curve.Evaluate(playTimer / this._duration));
+            if (_isPlaying) playTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        Lerp(this._to, this._to, 0);
+
+        _isPlaying = false;
+        _callback?.Invoke();
+        _callback = null;
+    }
+
+    private Vector3 GetFrom()
+    {
+        Vector3 from = Vector3.zero;
+
+        switch (this._type)
+        {
+            case TweenType.LocalPosition:
+                from = this._target.transform.localPosition;
+                break;
+            case TweenType.Position:
+                from = this._target.transform.position;
+                break;
+            case TweenType.Scale:
+                from = this._target.transform.localScale;
+                break;
+            case TweenType.Rotation:
+                from = this._target.transform.rotation.eulerAngles;
+                break;
+            case TweenType.LocalRotation:
+                from = this._target.transform.localRotation.eulerAngles;
+                break;
+        }
+        return from;
+    }
+
+    private void Lerp(Vector3 a, Vector3 b, float t)
+    {
+        switch (this._type)
+        {
+            case TweenType.LocalPosition:
+                this._target.transform.localPosition = Vector3.Lerp(a, b, t);
+                break;
+            case TweenType.Position:
+                this._target.transform.position = Vector3.Lerp(a, b, t);
+                break;
+            case TweenType.Scale:
+                this._target.transform.localScale = Vector3.Lerp(a, b, t);
+                break;
+            case TweenType.Rotation:
+                this._target.transform.rotation = Quaternion.Euler(Vector3.Lerp(a, b, t));
+                break;
+            case TweenType.LocalRotation:
+                this._target.transform.localRotation = Quaternion.Euler(Vector3.Lerp(a, b, t));
+                break;
+        }
+    }
+}
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -14,117 +14,6 @@ public enum TweenType
 
 public class Tweener
 {
-    public class Tween
-    {
-        private Transform _target = null;
-        private TweenType _type = TweenType.LocalPosition;
-        private Vector3 _from = Vector3.zero;
-        private Vector3 _to = Vector3.zero;
-        private Action _callback = null;
-        private AnimationCurve _curve = null;
-        private float _duration = 0.0f;
-        private bool _isPlaying = false;
-
-        public Tween(Transform target, TweenType type, Vector3 to, float duration, Action cb = null, EasingType easyType = EasingType.Linear)
-        {
-            this._target = target;
-            this._type = type;
-            this._to = to;
-            this._duration = duration;
-            this._callback = cb;
-            this._curve = CurveGenerator.Create(easyType);
-        }
-
-        public void AddListener(Action cb)
-        {
-            _callback += cb;
-        }
-
-        public bool IsPlaying()
-        {
-            return _isPlaying;
-        }
-
-        public void Pause()
-        {
-            _isPlaying = false;
-        }
-
-        public void Resume()
-        {
-            _isPlaying = true;
-        }
-
-        public IEnumerator Execution()
-        {
-            _isPlaying = true;
-            this._from = GetFrom();
-            float timer = 0.0f;
-
-            while (timer <= this._duration)
-            {
-                Lerp(this._from, this._to, this._curve.Evaluate(timer / this._duration));
-                if (_isPlaying) timer += Time.deltaTime;
-                yield return null;
-            }
-
-            Lerp(this._to, this._to, 0);
-
-            _isPlaying = false;
-            _callback?.Invoke();
-            _callback = null;
-        }
-
-        private Vector3 GetFrom()
-        {
-            Vector3 from = Vector3.zero;
-
-            switch (this._type)
-            {
-                case TweenType.LocalPosition:
-                    from = this._target.transform.localPosition;
-                    break;
-                case TweenType.Position:
-                    from = this._target.transform.position;
-                    break;
-                case TweenType.Scale:
-                    from = this._target.transform.localScale;
-                    break;
-                case TweenType.Rotation:
-                    from = this._target.transform.rotation.eulerAngles;
-                    break;
-                case TweenType.LocalRotation:
-                    from = this._target.transform.localRotation.eulerAngles;
-                    break;
-            }
-            return from;
-        }
-
-        private void Lerp(Vector3 a, Vector3 b, float t)
-        {
-            switch (this._type)
-            {
-                case TweenType.LocalPosition:
-                    this._target.transform.localPosition = Vector3.Lerp(a, b, t);
-                    break;
-                case TweenType.Position:
-                    this._target.transform.position = Vector3.Lerp(a, b, t);
-                    break;
-                case TweenType.Scale:
-                    this._target.transform.localScale = Vector3.Lerp(a, b, t);
-                    break;
-                case TweenType.Rotation:
-                    this._target.transform.rotation = Quaternion.Euler(Vector3.Lerp(a, b, t));
-                    break;
-                case TweenType.LocalRotation:
-                    this._target.transform.localRotation = Quaternion.Euler(Vector3.Lerp(a, b, t));
-                    break;
-            }
-        }
-
-        
-    }
-
     public bool IsPlaying { get; private set; }
 
     private Queue<Tween> _tweenQueue = new Queue<Tween>();
@@ -132,19 +21,19 @@ public class Tweener
     private Tween _currentTween = null;
     private IEnumerator _coroutine = null;
 
-    public void Play(MonoBehaviour target, TweenType type, Vector3 to, float duration, Action cb = null, EasingType easyType = EasingType.Linear)
+    public void Play(MonoBehaviour target, TweenType type, Vector3 to, float duration, Action callback = null, EasingType easyType = EasingType.Linear, float delay = 0)
     {
-        this.Play(target.transform, type, to, duration, cb, easyType);
+        this.Play(target.transform, type, to, duration, callback, easyType, delay);
     }
 
-    public void Play(GameObject target, TweenType type, Vector3 to, float duration, Action cb = null, EasingType easyType = EasingType.Linear)
+    public void Play(GameObject target, TweenType type, Vector3 to, float duration, Action callback = null, EasingType easyType = EasingType.Linear, float delay = 0)
     {
-        this.Play(target.transform, type, to, duration, cb, easyType);
+        this.Play(target.transform, type, to, duration, callback, easyType, delay);
     }
 
-    public void Play(Transform target, TweenType type, Vector3 to, float duration, Action cb = null, EasingType easyType = EasingType.Linear)
+    public void Play(Transform target, TweenType type, Vector3 to, float duration, Action callback = null, EasingType easyType = EasingType.Linear, float delay = 0)
     {
-        Tween tween = new Tween(target, type, to, duration, cb, easyType);
+        Tween tween = new Tween(target, type, to, duration, callback, easyType, delay);
         this.Play(tween);
     }
 
