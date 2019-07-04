38e252116e829c03e8c2f6a2a93031f1b75fdae5
Won Pyoung Choi <cwp10@msn.com>
2019-07-04 16:31:31 +0900

    Pause, Resume, Stop 추가

--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -16,14 +16,14 @@ public class Tweener
 {
     public class Tween
     {
-        Transform _target = null;
-
+        private Transform _target = null;
         private TweenType _type = TweenType.LocalPosition;
         private Vector3 _from = Vector3.zero;
         private Vector3 _to = Vector3.zero;
         private Action _callback = null;
         private AnimationCurve _curve = null;
         private float _duration = 0.0f;
+        private bool _isPlaying = false;
 
         public Tween(Transform target, TweenType type, Vector3 to, float duration, Action cb = null, EasingType easyType = EasingType.Linear)
         {
@@ -40,25 +40,39 @@ public class Tweener
             _callback += cb;
         }
 
+        public bool IsPlaying()
+        {
+            return _isPlaying;
+        }
+
+        public void Pause()
+        {
+            _isPlaying = false;
+        }
+
+        public void Resume()
+        {
+            _isPlaying = true;
+        }
+
         public IEnumerator Execution()
         {
+            _isPlaying = true;
             this._from = GetFrom();
             float timer = 0.0f;
 
             while (timer <= this._duration)
             {
                 Lerp(this._from, this._to, this._curve.Evaluate(timer / this._duration));
-                timer += Time.deltaTime;
+                if (_isPlaying) timer += Time.deltaTime;
                 yield return null;
             }
 
             Lerp(this._to, this._to, 0);
 
-            if (_callback != null)
-            {
-                _callback();
-                _callback = null;
-            }
+            _isPlaying = false;
+            _callback?.Invoke();
+            _callback = null;
         }
 
         private Vector3 GetFrom()
@@ -68,19 +82,19 @@ public class Tweener
             switch (this._type)
             {
                 case TweenType.LocalPosition:
-                    from = this._target.localPosition;
+                    from = this._target.transform.localPosition;
                     break;
                 case TweenType.Position:
-                    from = this._target.position;
+                    from = this._target.transform.position;
                     break;
                 case TweenType.Scale:
-                    from = this._target.localScale;
+                    from = this._target.transform.localScale;
                     break;
                 case TweenType.Rotation:
-                    from = this._target.rotation.eulerAngles;
+                    from = this._target.transform.rotation.eulerAngles;
                     break;
                 case TweenType.LocalRotation:
-                    from = this._target.localRotation.eulerAngles;
+                    from = this._target.transform.localRotation.eulerAngles;
                     break;
             }
             return from;
@@ -91,27 +105,42 @@ public class Tweener
             switch (this._type)
             {
                 case TweenType.LocalPosition:
-                    this._target.localPosition = Vector3.Lerp(a, b, t);
+                    this._target.transform.localPosition = Vector3.Lerp(a, b, t);
                     break;
                 case TweenType.Position:
-                    this._target.position = Vector3.Lerp(a, b, t);
+                    this._target.transform.position = Vector3.Lerp(a, b, t);
                     break;
                 case TweenType.Scale:
-                    this._target.localScale = Vector3.Lerp(a, b, t);
+                    this._target.transform.localScale = Vector3.Lerp(a, b, t);
                     break;
                 case TweenType.Rotation:
-                    this._target.rotation = Quaternion.Euler(Vector3.Lerp(a, b, t));
+                    this._target.transform.rotation = Quaternion.Euler(Vector3.Lerp(a, b, t));
                     break;
                 case TweenType.LocalRotation:
-                    this._target.localRotation = Quaternion.Euler(Vector3.Lerp(a, b, t));
+                    this._target.transform.localRotation = Quaternion.Euler(Vector3.Lerp(a, b, t));
                     break;
             }
         }
+
+        
     }
+
+    public bool IsPlaying { get; private set; }
 
     private Queue<Tween> _tweenQueue = new Queue<Tween>();
     private bool _isReady = true;
     private Tween _currentTween = null;
+    private IEnumerator _coroutine = null;
+
+    public void Add(MonoBehaviour target, TweenType type, Vector3 to, float duration, Action cb = null, EasingType easyType = EasingType.Linear)
+    {
+        this.Add(target.transform, type, to, duration, cb, easyType);
+    }
+
+    public void Add(GameObject target, TweenType type, Vector3 to, float duration, Action cb = null, EasingType easyType = EasingType.Linear)
+    {
+        this.Add(target.transform, type, to, duration, cb, easyType);
+    }
 
     public void Add(Transform target, TweenType type, Vector3 to, float duration, Action cb = null, EasingType easyType = EasingType.Linear)
     {
@@ -126,13 +155,34 @@ public class Tweener
         this.Next();
     }
 
+    public void Pause()
+    {
+        if (_currentTween != null) _currentTween.Pause();
+    }
+
+    public void Resume()
+    {
+        if (_currentTween != null) _currentTween.Resume();
+    }
+
+    public void Stop()
+    {
+        if (_coroutine != null)
+        {
+            _tweenQueue.Clear();
+            CoroutineHandler.StopStaticCoroutine(_coroutine);
+            _isReady = true;
+        }
+    }
+
     private void Next()
     {
         if (_tweenQueue.Count > 0 && _isReady)
         {
             _isReady = false;
             _currentTween = _tweenQueue.Dequeue();
-            CoroutineHandler.StartStaticCoroutine(_currentTween.Execution());
+            _coroutine = _currentTween.Execution();
+            CoroutineHandler.StartStaticCoroutine(_coroutine);
         }
     }
 
