342cdd21f2fbf262968932d3a65507fe2d079ca6
Won Pyoung Choi <cwp10@msn.com>
2019-07-04 17:22:47 +0900

    샘플 수정

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,16 +2,37 @@
 
 public class Main : MonoBehaviour
 {
+    private Tweener _tweener = new Tweener();
+
     void Start()
     {
-        Tweener tweener = new Tweener();
-        tweener.Add(this.transform, TweenType.LocalPosition, new Vector3(2, 2, 0), 3, cb: OnEnd, easyType: EasingType.BounceEaseOutIn);
-        tweener.Add(this.transform, TweenType.LocalPosition, new Vector3(1, -2, 0), 1, cb: OnEnd, easyType: EasingType.SineEaseIn);
-        tweener.Add(this.transform, TweenType.Rotation, new Vector3(0, 180, 0), 3f, cb: OnEnd, easyType: EasingType.QuadEaseIn);
+        _tweener.Play(this, TweenType.LocalPosition, new Vector3(2, 2, 0), 3, callback: () => {
+            _tweener.Play(this, TweenType.LocalPosition, new Vector3(1, -2, 0), 1, callback: () => {
+                _tweener.Play(this, TweenType.Rotation, new Vector3(0, 180, 0), 3f, callback: OnEnd, easyType: EasingType.QuadEaseIn);
+            }, easyType: EasingType.SineEaseIn, delay: 5.0f);
+        }, easyType: EasingType.BounceEaseOutIn);
     }
 
     void OnEnd()
     {
         Debug.Log("OnEnd");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _tweener.Pause();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _tweener.Resume();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _tweener.Stop();
+        }
     }
 }
