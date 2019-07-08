4ad8096d90186153615946d3dd5c4d25b8a85282
Won Pyoung Choi <cwp10@msn.com>
2019-07-08 13:26:52 +0900

    Setter 로 프로퍼티를 설정할 수 있도록 변경

--- Assets/Scripts/TweenControl.cs
+++ Assets/Scripts/TweenControl.cs
@@ -6,15 +6,16 @@ public class TweenControl : MonoBehaviour
 {
     [Serializable] public class Callback: UnityEvent {}
 
-    public Vector3 origin = Vector3.zero;
-    public Vector3 target = Vector3.zero;
-    public float duration = 0.0f;
-    public int loopCount = 0;
-    public float delay = 0.0f;
-    public bool isAuto = false;
-    public TweenType tweenType = TweenType.LocalPosition;
-    public EasingType easingType = EasingType.Linear;
-    public Callback onComplete = null;
+    [SerializeField] private Transform target_ = null;
+    [SerializeField] private Vector3 from_ = Vector3.zero;
+    [SerializeField] private Vector3 to_ = Vector3.zero;
+    [SerializeField] private float duration_ = 0.0f;
+    [SerializeField] private int loopCount_ = 0;
+    [SerializeField] private float delay_ = 0.0f;
+    [SerializeField] private bool isAuto = false;
+    [SerializeField] private TweenType tweenType_ = TweenType.LocalPosition;
+    [SerializeField] private EasingType easingType_ = EasingType.Linear;
+    [SerializeField] private Callback onComplete_ = null;
 
     private Tweener _tweener = new Tweener();
 
@@ -23,14 +24,62 @@ public class TweenControl : MonoBehaviour
         if (isAuto) this.Play();
     }
 
+    public void SetTarget(Transform target)
+    {
+        this.target_ = target;
+    }
+
+    public void SetFrom(Vector3 from)
+    {
+        this.from_ = from;
+    }
+
+    public void SetTo(Vector3 to)
+    {
+        this.to_ = to;
+    }
+
+    public void SetDuration(float time)
+    {
+        this.duration_ = time;
+    }
+
+    public void SetDelay(float time)
+    {
+        this.delay_ = time;
+    }
+
+    public void SetLoopCount(int count)
+    {
+        this.loopCount_ = count;
+    }
+
+    public void SetTweenType(TweenType tweenType)
+    {
+        this.tweenType_ = tweenType;
+    }
+
+    public void SetEasingType(EasingType easingType)
+    {
+        this.easingType_ = easingType;
+    }
+
+    public void SetCompleteCallback(UnityAction action)
+    {
+        this.onComplete_.AddListener(action);
+    }
+
     public void Play()
     {
         this.Reset();
 
+        Debug.Assert(target_ != null, "target is null");
+
         _tweener.Play(
-            transform, tweenType, target, duration, () => {
-                onComplete?.Invoke();
-            }, easingType, loopCount, delay);
+            target_, tweenType_, to_, duration_, () => {
+                onComplete_?.Invoke();
+                onComplete_ = null;
+            }, easingType_, loopCount_, delay_);
     }
 
     public void Pause()
@@ -50,22 +99,22 @@ public class TweenControl : MonoBehaviour
 
     private void Reset()
     {
-        switch (tweenType)
+        switch (tweenType_)
         {
             case TweenType.LocalPosition:
-                transform.localPosition = origin;
+                transform.localPosition = from_;
                 break;
             case TweenType.Position:
-                transform.position = origin;
+                transform.position = from_;
                 break;
             case TweenType.Scale:
-                transform.localScale = origin;
+                transform.localScale = from_;
                 break;
             case TweenType.Rotation:
-                transform.rotation = Quaternion.Euler(origin);
+                transform.rotation = Quaternion.Euler(from_);
                 break;
             case TweenType.LocalRotation:
-                transform.localRotation = Quaternion.Euler(origin);
+                transform.localRotation = Quaternion.Euler(from_);
                 break;
         }
     }
